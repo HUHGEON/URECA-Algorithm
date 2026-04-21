e4aafd70e43dabe85a0d22541b4cb100da69370c
HUHGEON <90556124+HUHGEON@users.noreply.github.com>
2026-04-21 14:47:38 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17696 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,12 +7,14 @@ public class Main {
         int L = sc.nextInt();
         String hash = sc.next();
 
-        int hashCode = 0;
-        int M = 1234567891;
+        long hashCode = 0;
+        long M = 1234567891;
+        long pow = 1;
 
         for(int i= 0; i < L; i++){
             int code = hash.charAt(i) - 'a' + 1;
-            hashCode += (code * (int)(Math.pow(31 , i)) % M);
+            hashCode += (code * pow % M);
+            pow = pow * 31 % M;
         }
 
         System.out.println(hashCode);
