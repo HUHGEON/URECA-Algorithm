ef5d326867d0125dd5b51e3a0c8401d917c5d9eb
HUHGEON <90556124+HUHGEON@users.noreply.github.com>
2026-04-23 13:40:16 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 56852 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i < n + 1; i++){
+            q.add(i);
+        }
+
+        while (q.size() > 1){
+            q.poll();
+            int num = q.poll();
+            q.add(num);
+        }
+        System.out.println(q.poll());
+
+
+    }
+}
+
